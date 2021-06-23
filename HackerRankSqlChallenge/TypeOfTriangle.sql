48ddaa67c8e1f6ab5438ebd594bcdbc7b9584b69
JOSEPH LOPEZ <jprlopez@hotmail.com>
2021-06-22 20:59:44 -0700

    Add project files.

---
+++ HackerRankSqlChallenge/TypeOfTriangle.sql
@@ -0,0 +1,8 @@
+﻿SELECT 
+    CASE 
+        WHEN A+B <= C THEN 'Not A Triangle'
+        WHEN A = B AND A = C AND B = C THEN 'Equilateral'
+        WHEN A = B OR A = C OR B = C THEN 'Isosceles'
+        WHEN A <> B AND A <> C AND B <> C THEN 'Scalene'
+    END
+FROM TRIANGLES
