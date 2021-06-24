c4cc5c2c04d97633ab868ac50fe2db39e0f58ffa
JOSEPH LOPEZ <jprlopez@hotmail.com>
2021-06-24 07:38:35 -0700

    Occupations Pivot challenge

---
+++ HackerRankSqlChallenge/Occupations.sql
@@ -0,0 +1,10 @@
+﻿SELECT
+    [Doctor], [Professor], [Singer], [Actor]
+FROM
+(
+    SELECT Name, Occupation, ROW_NUMBER() OVER (PARTITION BY OCCUPATION ORDER BY NAME) AS RowOrder FROM Occupations
+) AS tempTable
+PIVOT
+(
+    MAX(Name) FOR Occupation IN ([Doctor], [Professor], [Singer], [Actor])
+) AS pvt;
