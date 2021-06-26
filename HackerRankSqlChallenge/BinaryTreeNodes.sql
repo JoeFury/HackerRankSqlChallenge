84d101c71a178201861e2bd44007bd7e3470ef89
JOSEPH LOPEZ <jprlopez@hotmail.com>
2021-06-25 17:54:28 -0700

    binary tree

---
+++ HackerRankSqlChallenge/BinaryTreeNodes.sql
@@ -0,0 +1,19 @@
+﻿IF EXISTS(SELECT * FROM sys.tables WHERE name = 'BST')
+	DROP TABLE BST
+
+CREATE TABLE BST(N int, P int)
+
+
+INSERT INTO BST VALUES 
+(1,2),(3,2),(6,8),(9,8),(2,5),(8,5), (5, null)
+
+
+SELECT 
+	N
+,	CASE 
+		WHEN P IS NULL THEN 'Root'
+		WHEN (SELECT COUNT(*) FROM BST sub WHERE sub.P = b.N) > 0 THEN 'Inner' 
+		ELSE 'Leaf'
+	END 
+FROM BST b
+ORDER BY N;
--- HackerRankSqlChallenge/Occupations.sql
+++ HackerRankSqlChallenge/Occupations.sql
@@ -1,4 +1,23 @@
-﻿SELECT
+﻿IF EXISTS(SELECT * FROM sys.tables WHERE name = 'Occupations')
+	DROP TABLE Occupations
+
+CREATE TABLE Occupations(Name VARCHAR(100), Occupation VARCHAR(100))
+
+
+INSERT INTO Occupations VALUES 
+('Samantha', 'Doctor'),
+('Julia','Actor'),
+('Maria','Actor'),
+('Meera','Singer'),
+('Ashely','Professor'),
+('Ketty','Professor'),
+('Christeen','Professor'),
+('Jane','Actor'),
+('Jenny', 'Doctor')
+
+
+
+SELECT
     [Doctor], [Professor], [Singer], [Actor]
 FROM
 (
@@ -7,4 +26,4 @@ FROM
 PIVOT
 (
     MAX(Name) FOR Occupation IN ([Doctor], [Professor], [Singer], [Actor])
-) AS pvt;
+) AS pvt
