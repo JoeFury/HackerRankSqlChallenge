b71d98550677e9079577003ea225e49bb4e567af
JOSEPH LOPEZ <jprlopez@hotmail.com>
2021-06-28 19:58:48 -0700

    Add solutions for Advanced Join (Placements, Project Planning, Symmetric Pairs)

---
+++ HackerRankSqlChallenge/Advanced Join/Placements.sql
@@ -0,0 +1,8 @@
+﻿SELECT s.Name 
+FROM Students s
+JOIN Friends f ON s.ID= f.ID
+JOIN Packages sp ON s.ID = sp.ID
+JOIN Packages fp ON f.Friend_Id = fp.ID
+AND sp.Salary < fp.Salary
+GROUP BY s.Name
+ORDER BY MAX(fp.Salary);
---
+++ HackerRankSqlChallenge/Advanced Join/SymmetricPairs.sql
@@ -0,0 +1,14 @@
+﻿WITH CTE 
+AS
+(
+    SELECT f1.X, f1.Y, ROW_NUMBER() OVER (ORDER BY f1.X) AS RowNumber
+    FROM Functions f1
+) 
+SELECT DISTINCT f1.X, f1.Y
+FROM CTE f1
+JOIN CTE f2
+    ON f1.X = f2.Y
+    AND f1.Y = f2.X
+    AND f1.RowNumber <> f2.RowNumber
+WHERE f1.X <= f1.Y
+ORDER BY f1.X;
