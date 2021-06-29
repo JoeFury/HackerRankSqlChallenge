72a4ce30a0633a1ba99d633ae224b1fa9e3513d4
JOSEPH LOPEZ <jprlopez@hotmail.com>
2021-06-28 17:52:46 -0700

    top earners aggregation

---
+++ HackerRankSqlChallenge/Aggregation/TopEarners.sql
@@ -0,0 +1,5 @@
+﻿;WITH MAX_COMP (max_comp) AS
+(SELECT MAX(months * salary) FROM Employee ee)
+SELECT MAX(max_comp), SUM(CASE WHEN (ee.months * ee.salary) = mc.max_comp THEN 1 ELSE 0 END) FROM Employee ee
+CROSS APPLY MAX_COMP mc
+;
